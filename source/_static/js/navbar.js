ce2bb29fec0f62ba20bfcb7ee02b9528be75d07b
xiao tang <2310296215@qq.com>
2022-01-14 09:23:13 +0800

    test_html

--- source/_static/js/navbar.js
+++ source/_static/js/navbar.js
@@ -12,33 +12,89 @@ if (!String.prototype.includes) {
     }
   };
 }
-
+function up(X){
+  var up_ = X.getElementsByTagName("svg")
+  up_[0].outerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="currentColor" class="bi bi-caret-up-fill" viewBox="0 0 16 16">
+                      <path d="m7.247 4.86-4.796 5.481c-.566.647-.106 1.659.753 1.659h9.592a1 1 0 0 0 .753-1.659l-4.796-5.48a1 1 0 0 0-1.506 0z"/>
+                      </svg>`
+  up_[0].style.color = "#03a9f4"
+}
+function down(Y){
+  var up_ = Y.getElementsByTagName("svg")
+  up_[0].outerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="currentColor" class="bi bi-caret-down-fill" viewBox="0 0 16 16">
+                      <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"/>
+                      </svg>`
+  up_[0].style.color = "#000"
+}
 function addNavbar() {
-  var navbarHtml = "" +
-    "  <div id=\"lux-global-navbar\" class=\"lux-navbar-container\">\n" +
-    "    <div class=\"lux-navbar\">\n" +
-    "      <ul>\n" +
-    "        <li><a href=\"https://luxonis.com\">\n" +
-    "          <img src=\"https://discuss.luxonis.com/assets/logo-fuhmgk6r.png\" alt=\"Luxonis\">\n" +
-    "        </a></li>\n" +
-    "        <li><a href=\"https://docs.luxonis.com/\" class=\"lux-navbar-active\">Documentation</a></li>\n" +
-    "        <li><a href=\"https://discuss.luxonis.com/\">Discuss</a></li>\n" +
-    "        <li><a href=\"https://luxonis.com/careers\">Careers</a></li>\n" +
-    "        <li><a href=\"https://luxonis.com/team\">Team</a></li>\n" +
-    "        <li><a href=\"https://luxonis.com/contact\">Contact</a></li>\n" +
-    "        <li><a href=\"http://shop.luxonis.com\">Shop</a></li>\n" +
-    "      </ul>\n" +
-    "    </div>\n" +
-    "  </div>\n" +
-    "  <div id=\"lux-doc-navbar\" class=\"lux-navbar-container\">\n" +
-    "    <div class=\"lux-navbar\">\n" +
-    "      <ul>\n" +
-    "        <li><a href=\"/en/latest/\">Main</a></li>\n" +
-    "        <li><a href=\"/projects/api/\">API</a></li>\n" +
-    "        <li><a href=\"/projects/hardware/\">Hardware</a></li>\n" +
-    "      </ul>\n" +
-    "    </div>\n" +
-    "  </div>"
+  var navbarHtml = `      <div class="navbar">
+  <div class="navbar_menu">
+      <div class="img">
+          <a href="https://www.oakchina.cn/">
+              <img src="https://www.oakchina.cn/wp-content/uploads/2021/12/cropped-OAKChina-01.png" alt="ArduCam">
+          </a>
+      </div>
+      <div class="menu_list">
+          <div class="drop-menu" onmouseover="up(this)" onmouseout="down(this)">
+              <div class="hover-btn"> <a href="https://www.oakchina.cn/#products">产品
+                  <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="currentColor" class="bi bi-caret-down-fill" viewBox="0 0 16 16">
+                      <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"/>
+                  </svg>
+              </a></div>
+              <div class="drop-content">
+                  <a href="https://www.oakchina.cn/selection-guide/">选购指南</a>
+                  <a href="https://www.oakchina.cn/products/">所有产品</a>
+              </div>
+          </div>
+          <div class="drop-menu" onmouseover="up(this)" onmouseout="down(this)">
+              <div class="hover-btn"><a href="https://www.oakchina.cn/#solutions">解决方案
+                  <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="currentColor" class="bi bi-caret-down-fill" viewBox="0 0 16 16">
+                      <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"/>
+                  </svg>
+              </a></div>
+              <div class="drop-content">
+                  <a href="https://www.oakchina.cn/heavy-industry/">工业智能化</a>
+                  <a href="https://www.oakchina.cn/robotic-drones/">机器人无人机</a>
+                  <a href="https://www.oakchina.cn/security-surveillance-with-oak/">安防监控</a>
+                  <a href="https://www.oakchina.cn/smart-drive/">智能驾驶</a>
+                  <a href="https://www.oakchina.cn/medical-health-with-oak/">医疗大健康</a>
+                  <a href="https://www.oakchina.cn/education-with-oak/">编程教育</a>
+              </div>
+          </div>
+          <div class="drop-menu" onmouseover="up(this)" onmouseout="down(this)">
+              <div class="hover-btn"><a class="hover-btn-active" href="https://www.oakchina.cn/#docs" >教程
+                  <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="currentColor" class="bi bi-caret-down-fill" viewBox="0 0 16 16">
+                      <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"/>
+                  </svg>
+              </a></div>
+              <div class="drop-content">
+                  <a href="https://docs.oakchina.cn/en/latest/">使用教程</a>
+                  <a href="https://www.oakchina.cn/tech-faq/">问题排错</a>
+                  <a href="https://www.oakchina.cn/oak-opensource-projects/">开源项目</a>
+                  <a href="https://www.oakchina.cn/free-ai-model/">免费预训练模型</a>
+              </div>
+          </div>
+          <div class="drop-menu">
+              <div class="hover-btn"><a href="https://www.oakchina.cn/blog/">文章</a></div>
+          </div>
+
+          <div class="drop-menu">
+              <div class="hover-btn"><a href="https://njpc.taobao.com/">购买</a></div>
+          </div>
+          <div class="drop-menu">
+              <div class="hover-btn"><a href="https://www.oakchina.cn/#contacts">联系我们</a></div>
+          </div>
+      </div> 
+  </div>
+</div>
+<div id="lux-doc-navbar" class="lux-navbar-container">
+    <div class="lux-navbar">
+      <ul>
+        <li><a href="/en/latest/" class="lux-navbar-active-" >Main</a></li>
+        <li><a href="/projects/api/" >API</a></li>
+      </ul>
+    </div>
+  </div>`
   document.body.insertAdjacentHTML( 'afterbegin', navbarHtml );
 }
 
